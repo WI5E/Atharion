d5b85b48096b7b386fb2724b17689bce26e71408
WI5E <wi5e.64a@gmail.com>
2020-01-01 00:40:10 -0800

    quest no longer has state implementation

--- commons/src/main/java/com/atharion/commons/quests/AbstractQuest.java
+++ commons/src/main/java/com/atharion/commons/quests/AbstractQuest.java
@@ -1,14 +1,13 @@
 package com.atharion.commons.quests;
 
 import com.atharion.commons.quests.requirements.Requirement;
-import com.atharion.commons.statemachine.StateSeries;
 import lombok.Getter;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Getter
-public abstract class AbstractQuest extends StateSeries implements Quest {
+public abstract class AbstractQuest implements Quest {
 
     private final String name;
 
